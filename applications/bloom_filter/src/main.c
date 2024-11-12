7413c38d4027544ef81ca803d4c2f6758163b635
dakshinatharindu <tharinduokd@gmail.com>
2024-11-13 00:54:29 +0530

    add bloom_filter applicaiton

---
+++ applications/bloom_filter/src/main.c
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2012-2014 Wind River Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+#include <zephyr/kernel.h>
+
+#define STACKSIZE 1024
+#define PRIORITY 7
+
+int bloom_filter_main(void);
+
+void thread1(void)
+{
+	// while (1) {
+	// 	printf("Hello from thread1\n");
+	// 	k_msleep(2000);
+	// }
+
+    bloom_filter_main();
+}
+
+void thread2(void)
+{
+	while (1) {
+		// printf("Hello from thread2\n");
+		k_msleep(1000);
+	}
+}
+
+K_THREAD_DEFINE(thread1_id, STACKSIZE, thread1, NULL, NULL, NULL,
+		PRIORITY, 0, 0);
+K_THREAD_DEFINE(thread2_id, STACKSIZE, thread2, NULL, NULL, NULL,
+		PRIORITY, 0, 0);
