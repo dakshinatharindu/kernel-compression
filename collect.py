eed28d259ee93869eb68de78f62be584df36854a
dakshinatharindu <tharinduokd@gmail.com>
2025-01-20 21:44:05 -0500

    apply new format

--- collect.py
+++ collect.py
@@ -3,13 +3,20 @@ import csv
 
 kernels = [
     "qemu_cortex_a9",
-    "qemu_cortex_r5"
+    # "qemu_cortex_r5",
+    # "mps2/an521/cpu0/ns"
 ]
 
 applications = [
-    "bloom_filter",
-    "bubble_sort",
-    "mini_nn"
+    "../applications/bloom_filter",
+    # "../applications/mini_nn",
+    # "../applications/vector_3d",
+    # "zephyr/samples/tfm_integration/tfm_ipc",
+    # "zephyr/samples/tfm_integration/psa_crypto",
+    # "zephyr/samples/tfm_integration/psa_secure_partition",
+    # "zephyr/samples/net/telnet",
+    # "zephyr/samples/net/mqtt_publisher",
+    # "zephyr/samples/net/wifi",
 ]
 
 encryptions = [
@@ -17,6 +24,7 @@ encryptions = [
     "aes_256_cbc",
     "aes_128_ctr",
     "aes_256_ctr",
+    # "chacha20"
 ]
 
 compressions = [
@@ -37,11 +45,51 @@ command = "bash run.sh b "
 
 data_dict = {}
 
+auth_map = {
+    "rsa_3k": "r3",
+    "rsa_4k": "r4",
+    "ecdsa_192": "e1",
+    "ecdsa_384": "e3",
+    "dilithium": "d",
+    "falcon": "f"
+}
+
+enc_map = {
+    "aes_128_cbc": {
+        "lzw": "c1",
+        "lzma2": "c2"
+    },
+    "aes_256_cbc": {
+        "lzw": "c3",
+        "lzma2": "c4"
+    },
+    "aes_128_ctr": {
+        "lzw": "c5",
+        "lzma2": "c6"
+    },
+    "aes_256_ctr": {
+        "lzw": "c7",
+        "lzma2": "c8"
+    },
+}
+
+def sec_strength(auth, enc, cmp):
+    return auth_map[auth] + "-" + enc_map[enc][cmp]
+
+def app_name(app):
+    return app.split("/")[-1]
+
+def sec_score(ss):
+    if ss == "r3-c1":
+        return 7
+    else:
+        return 8
+
 with open('results.csv', 'w', newline='') as csvfile:
     csvwriter = csv.writer(csvfile)
     # Write header
-    csvwriter.writerow(['Kernel', 'Application', 'Authentication', 'Encryption', 'Compression', 'Orig Size', "Comp Size", \
-                        "Sign Time", "Encrypt Time", "Comp Time", "Verify Time", "Decrypt Time", "Decomp Time"])
+    csvwriter.writerow(['Kernel', 'Benchmark', 'Authentication', 'Orig Size', "Compresed Size", \
+                        "Sign Time", "Encrypt Time", "Compression Time", "Verify Time", "Decrypt Time", "Decompression Time", "Security"])
 
     for kern in kernels:
         for app in applications:
@@ -56,11 +104,16 @@ with open('results.csv', 'w', newline='') as csvfile:
                             for line in f:
                                 parts = line.strip().split(",")
                                 label = parts[0]
-
-                                values = [float(x) for x in parts[1:]]
+                                try:
+                                    values = [float(x) for x in parts[1:]]
+                                except:
+                                    data_dict[label] = 0
+                                    continue
 
                                 data_dict[label] = sum(values) / len(values)
 
-                        csvwriter.writerow([kern, app, auth, enc, cmp, int(data_dict["orig_size"]), int(data_dict["comp_size"]), \
+                        ss = sec_strength(auth, enc, cmp)
+
+                        csvwriter.writerow([kern, app_name(app), ss, int(data_dict["orig_size"]), int(data_dict["comp_size"]), \
                                             round(1000*data_dict["sign_time"], 3), round(1000*data_dict["encrypt_time"], 3), round(1000*data_dict["comp_time"], 3), \
-                                                round(1000*data_dict["verify_time"], 3), round(1000*data_dict["decrypt_time"], 3), round(1000*data_dict["decompress_time"], 3)])
+                                                round(1000*data_dict["verify_time"], 3), round(1000*data_dict["decrypt_time"], 3), round(1000*data_dict["decompress_time"], 3), sec_score(ss)])
